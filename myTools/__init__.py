7fda9ac014c63965306a56fc28900a54ac4be7ef
tmsmolnar <73756619+tmsmolnar@users.noreply.github.com>
2021-05-06 12:58:45 +0200

    added scrapeSeriesLengths()

--- myTools/__init__.py
+++ myTools/__init__.py
@@ -4,5 +4,5 @@
 # __init__.py
 
 from .dataTools import readNetflixDataset, episodePerDay, titlePerGenre, titleType, titleTypeUnique
-from .scrapingTools import scrapeBreakingBadRatings, scrapeHomelandRatings, scrapeMovieLengths
+from .scrapingTools import scrapeBreakingBadRatings, scrapeHomelandRatings, scrapeMovieLengths, scrapeSeriesLengths
 from .visualizationTools import watchingHabit, mostWatchedGenre, mostWatchedType, mostWatchedTypeUnique, busiestDay, heatMapBreakingBad, heatMapHomeland
--- myTools/scrapingTools.py
+++ myTools/scrapingTools.py
@@ -56,9 +56,8 @@ def scrapeHomelandRatings():
 
     return dataFrame
 
-def scrapeMovieLengths():
 
-    ### Not yet working ###
+def scrapeMovieLengths():
 
     movieLengths = []
 
@@ -82,6 +81,36 @@ def scrapeMovieLengths():
         movieLengths.append(data)
 
     dataFrame = pd.DataFrame(movieLengths, columns=[
+        'title', 'runtime'])
+    dataFrame = dataFrame.fillna(0)
+
+    return dataFrame
+
+
+def scrapeSeriesLengths():
+
+    seriesLengths = []
+
+    seriesHTML = 'https://www.imdb.com/list/ls500780679/'
+    response = requests.get(seriesHTML)
+    html = response.content
+    pageHTML = BeautifulSoup(html, 'html.parser')
+
+    series = pageHTML.find_all('div', {'class': 'lister-item mode-detail'})
+
+    for s in series:
+
+        title = s.find('h3', {'class': 'lister-item-header'})
+        title = title.text.split("\n")
+
+        runtime = s.find('span', {'class': 'runtime'})
+        runtime = runtime.text.split(" min")
+
+        data = [str(title[2]), int(runtime[0])]
+
+        seriesLengths.append(data)
+
+    dataFrame = pd.DataFrame(seriesLengths, columns=[
         'title', 'runtime'])
     dataFrame = dataFrame.fillna(0)
 
