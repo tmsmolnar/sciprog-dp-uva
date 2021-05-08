c5eac69f4c00059fb5951ce391d5645a4af20f2c
tmsmolnar <73756619+tmsmolnar@users.noreply.github.com>
2021-05-08 20:46:33 +0200

    (dont change the order of imports in __init__)

--- myTools/__init__.py
+++ myTools/__init__.py
@@ -4,5 +4,5 @@
 # __init__.py
 
 from .scrapingTools import scrapeBreakingBadRatings, scrapeHomelandRatings, scrapeMovies, scrapeSeries
-from .visualizationTools import watchingHabit, mostWatchedGenre, mostWatchedType, mostWatchedTypeUnique, busiestDay, heatMapBreakingBad, heatMapHomeland
 from .dataTools import readNetflixDataset, episodePerDay, titlePerGenre, titleType, titleTypeUnique, seriesData, moviesData, concatSeriesMovies, cleanDataFrame, mapRuntime, mapGenre, mapType
+from .visualizationTools import watchingHabit, mostWatchedGenre, mostWatchedType, mostWatchedTypeUnique, busiestDay, heatMapBreakingBad, heatMapHomeland
